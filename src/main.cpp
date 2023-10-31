ac8a5094cd9880c2a065801fde12a557c5848ee6
Paul Schaber <paul.schaber@student.tabyenskilda.se>
2023-10-31 18:41:13 +0100

    fix of data frequency (I remembered), further printout fixes and addition of countdown before start.

--- src/main.cpp
+++ src/main.cpp
@@ -23,33 +23,26 @@ int dutyCycle = 3277;
 void setup() {
     Serial.begin(115200);
 
-// PWM signal for ESC to controll motorspeed
-    delay(5000);
+    // countdown before program starts doing things
+    for (int i = 0; i <= 5; i++) {
+        Serial.println(5-i);
+        delay(1000);
+    }
+
+    // PWM signal for ESC to controll motorspeed
     ledcAttachPin(ESC_CONTROLL_PIN, ESC_CONTROLL_CHANNEL);
     ledcSetup(ESC_CONTROLL_CHANNEL, 50, 16);
     ledcWrite(ESC_CONTROLL_CHANNEL, dutyCycle);
 
-// Setup for adc measuring the voltage and current passing to/ through the motor
-
-    Serial.println(__FILE__);
-    Serial.print("ADS1X15_LIB_VERSION: ");
-    Serial.println(ADS1X15_LIB_VERSION);
+    // Setup for adc measuring the voltage and current passing to/ through the motor
     adcConnection.begin(ADC_DATA_PIN, ADC_CLOCK_PIN);
     ADS.begin();
     ADS.setGain(0);
 
-// Setup for loadcell
-
+    // Setup for loadcell
     scale.begin(LOADCELL_DATA_PIN, LOADCELL_CLOCK_PIN);
 
-    if (scale.is_ready()) {
-        scale.set_scale();
-    } else {
-    Serial.println("HX711 not found.");
-    }
-
-    delay(5000);
-    // motor ramp up sequence, including code making measurements
+    // Motor ramp up sequence, including code making measurements
     while (dutyCycle < 6553) {
 
         // ADC code
@@ -57,16 +50,19 @@ void setup() {
         int16_t val_1 = ADS.readADC(1);
 
 
-        // prints out the values of the adc pins
+        // Prints out the values of the adc pins
         Serial.print("batteryVoltage:"); Serial.print((double) val_0 /1231);
-        Serial.print(":circuitCurrent:"); Serial.print('\t'); Serial.println(val_1 / 19);
+        Serial.print(":circuitCurrent:"); Serial.print((double) val_1 / 19);
+
+        // Prints out the value given by the loadcell
+        Serial.print(":loadCellValue:"); Serial.println(scale.read());
 
         // ESC code
         //Serial.print(dutyCycle);
-        for (int i = 0; i < 163; i++) {
+        for (int i = 0; i < 3; i++) {
             ledcWrite(ESC_CONTROLL_CHANNEL, dutyCycle);
             dutyCycle += 1;
-            delay(1000 / 163);
+            delay(50 / 3);
         }
     }
 }
@@ -87,6 +83,7 @@ void loop() {
     Serial.print("batteryVoltage:"); Serial.print((double) val_0 /1231);
     Serial.print(":circuitCurrent:"); Serial.print((double) val_1 / 19);
 
+    // Prints out the value given by the loadcell
     Serial.print(":loadCellValue:"); Serial.println(scale.read());
     delay(50);
 }
